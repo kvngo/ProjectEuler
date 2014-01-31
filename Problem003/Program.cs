053eac864a4e50483e1f28a5040d36cdda58e4bf
kvngo <kimngo@gmail.com>
2014-01-31 10:41:43 -0800

    Problem 1, 2 completed

---
+++ Problem003/Program.cs
@@ -0,0 +1,19 @@
+﻿//The prime factors of 13195 are 5, 7, 13 and 29.
+//
+//What is the largest prime factor of the number 600851475143 ?
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem003
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ ProjectEuler/Program.cs
@@ -0,0 +1,49 @@
+﻿/*
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+Find the sum of all the multiples of 3 or 5 below 1000.
+Trick: numbers like 15 are multiples of 3 and 5 so they shouldn't be counted twice.
+ * 
+ * */
+
+
+using System;
+using System.Collections.Generic;
+
+namespace ProjectEuler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int maxValue = 1000;
+            var numbersList = new HashSet<int>();
+            int temp = 0;
+            int total = 0;
+
+            // add multiples of 3's below 1000
+            temp = 3;
+            while (temp < maxValue)
+            {
+                numbersList.Add(temp);
+                temp+=3;
+            }
+
+            // add multiples of 5's below 1000
+            temp = 5;
+            while (temp < maxValue)
+            {
+                numbersList.Add(temp);
+                temp += 5;
+            }
+
+
+            foreach (var num in numbersList)
+                total += num;
+
+            Console.WriteLine("Total: " + total);
+
+
+
+        }
+    }
+}
